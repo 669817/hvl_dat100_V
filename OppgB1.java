a573ab828497ae6a9f853e3bc4652828fa0f2492
669817 <111427661+669817@users.noreply.github.com>
2022-08-30 17:02:56 +0200

    Add files via upload

--- OppgB1.java
+++ OppgB1.java
@@ -1,5 +1,21 @@
 package oppgaver300822;
 
+import static javax.swing.JOptionPane.*;
+
 public class OppgB1 {
 	
+	public static void main(String[] args) {
+		
+		String radius = showInputDialog("Skriv inn radiusen på kulen");
+		int rad = java.lang.Integer.parseInt(radius);
+		double rad3 = Math.pow(rad, 3) ;
+		double pi = Math.PI;
+		
+		int volum = (int) ((4 * pi * rad3)/3);
+		
+		String txt = "Volumet av kulen er: " + volum; 
+		
+		showMessageDialog(null, txt);
+		
+	}
 }
